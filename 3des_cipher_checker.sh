f9926cb3c2e9df1bf454b1244321b92a13f1ca35
habbdt@gmail.com <habbdt@gmail.com>
2021-03-19 18:28:01 -0400

    3des cripher

--- 3des_cipher_checker.sh
+++ 3des_cipher_checker.sh
@@ -5,8 +5,8 @@
 ######################################
 
 # error handling
-set -o pipefail  # trace ERR through pipes
-set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail
+set -o errtrace
 
 # user input
 INPUT_FILE=${1}
@@ -17,17 +17,17 @@ if [ "$#" -ne 1 ]
 then
     echo "Usage: 3des_cipher_checker.sh <input_file_hostname_ip> <cipher>"
     echo "input_file: list of the hostname or ip address"
-    echo "cipher: check support for specific support is enabled"
+    echo "cipher: check support for specific cipher is enabled (default -3DES)"
     exit
 fi
 
 # check if sslscan is installed or not
-
 if ! [ -x "$(command -v sslscan)" ]; then
   echo 'Error: sslscan is not installed.' >&2
   exit 1
 fi
 
+# cipher checker function
 function cipher_scanner() {
   while IFS= read -r line; do
       SSL_SCAN="$(sslscan --no-renegotiation --no-heartbleed --no-fallback --no-groups \
